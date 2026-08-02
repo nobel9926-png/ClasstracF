8041628b77a78cf05183be380ed0cae809eec37e
Bilisha Khatri <bilisha5126@student.ku.edu.np>
2026-08-02 21:10:48 +0545

    Add files via upload

---
+++ home_screen.h
@@ -0,0 +1,10 @@
+#ifndef HOME_SCREEN_H
+#define HOME_SCREEN_H
+//header guard vaihalyo
+#include "classtrac.h"
+
+void DrawHomeScreen(Screen *currentScreen, Font font, bool *exitRequested);
+
+
+
+#endif 
